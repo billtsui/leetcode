f0f65357b37096f345595ccaa9953be2e3a9e956
Bill Tsui <billkuentsui@outlook.com>
2021-08-10 21:26:22 +0800

    946.验证栈序列

---
+++ 946.验证栈序列.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode.cn id=946 lang=java
+ *
+ * [946] 验证栈序列
+ */
+
+// @lc code=start
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        if(pushed.length == 0 || popped.length == 0){
+            return false;
+        }
+
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        int n = pushed.length;
+        int l = 0;
+
+        for (int i = 0; i < n; i++) {
+            stack.push(pushed[i]);
+            while(!stack.empty() && (popped[l] == stack.peek())){
+                l++;
+                stack.pop();
+            }
+        }
+
+        if(stack.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
+// @lc code=end
+
