1371719ca61a9bfc61d26d63a4f5fa528f08493d
Bill Tsui <billkuentsui@outlook.com>
2021-08-10 20:30:33 +0800

    155.最小栈

---
+++ 155.最小栈.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode.cn id=155 lang=java
+ *
+ * [155] 最小栈
+ */
+
+// @lc code=start
+class MinStack {
+
+    Stack<Integer> stack = null;
+    Stack<Integer> minStack = null;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val < minStack.peek()) {
+            minStack.push(val);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+// @lc code=end
+
